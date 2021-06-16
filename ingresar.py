9d101c5d2454c035111cea02c93266be411811ee
Vania-Espinoza <72463438+Vania-Espinoza@users.noreply.github.com>
2021-06-15 22:00:29 -0400

    ingresar
    inicio de un formulario para ingresar los datos requeridos

---
+++ ingresar.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+from tkinter import messagebox
+
+class FormularioRegistro(tk.Tk):
+
+    def __init__(self):
+        super().__init__()
+
+        self.inicializar_gui()
+
+    def inicilializar_gui(self):
+        self.tittle('Validacion Datos')
+        self.minsize(400,300)
+
+def main():
+    app = FormularioRegistro()
+    app.mainloop()
+
+if __name__ == '__main__':
+    main()
